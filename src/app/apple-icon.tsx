a0b69188cca632847d0833e001f1591677b84acc
Asim Kamal <asimkamalk@gmail.com>
2026-09-03 23:45:00 +0500

    chore: implement dynamic Apple icon generation with site settings integration for improved Open Graph support

--- src/app/apple-icon.tsx
+++ src/app/apple-icon.tsx
@@ -1,1 +1,51 @@
-export { default, size, contentType, runtime, dynamic } from "./icon";
+import { ImageResponse } from "next/og";
+import { getSiteSettings } from "@/lib/data/site";
+import { resolveSiteIconUrl } from "@/lib/site-icon";
+
+export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
+export const size = { width: 180, height: 180 };
+export const contentType = "image/png";
+
+export default async function AppleIcon() {
+  const settings = await getSiteSettings();
+  const src = resolveSiteIconUrl(settings);
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          background: "#10251B",
+        }}
+      >
+        {src ? (
+          // eslint-disable-next-line @next/next/no-img-element
+          <img
+            src={src}
+            alt=""
+            width={180}
+            height={180}
+            style={{ width: 180, height: 180, objectFit: "contain" }}
+          />
+        ) : (
+          <div
+            style={{
+              color: "#F1F3EC",
+              fontSize: 48,
+              fontWeight: 700,
+              fontFamily: "sans-serif",
+            }}
+          >
+            KP
+          </div>
+        )}
+      </div>
+    ),
+    { ...size }
+  );
+}
